73a6476c33eaa150743ced822ec25483cfbaa634
Daniel Castilho <dan.castilho@gmail.com>
2021-06-11 20:51:34 -0400

    .

---
+++ src/main/java/io/tyny/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,18 @@
+package io.tyny.ppmtool.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import io.tyny.ppmtool.domain.Project;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+	@Override
+	default Iterable<Project> findAllById(Iterable<Long> ids) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+}
---
+++ src/main/java/io/tyny/ppmtool/web/ProjectController.java
@@ -0,0 +1,39 @@
+package io.tyny.ppmtool.web;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.tyny.ppmtool.domain.Project;
+import io.tyny.ppmtool.services.MapValidationErrorService;
+import io.tyny.ppmtool.services.ProjectService;
+
+@RestController
+@RequestMapping("/api/project")
+public class ProjectController {
+
+	@Autowired
+	private ProjectService projectService;
+
+	@Autowired
+	private MapValidationErrorService mapValidationErrorService;
+
+	@PostMapping("")
+	public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
+
+		ResponseEntity<?> errorMap = mapValidationErrorService.mapValidationService(result);
+		if (errorMap != null) {
+			return errorMap;
+		}
+
+		projectService.saveOrUpdateProject(project);
+		return new ResponseEntity<Project>(project, HttpStatus.CREATED);
+	}
+}
